49d661bea3358bbe3f17f973e795f7b99bfc61c5
DanielSLizhnyk <lucifer.dlo.69@gmail.com>
2022-06-08 16:07:53 +0300

    master

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', 'MainController@index');
 
-Route::get('/categories', function(){
-    return view('categories');
-});
+Route::get('/categories', 'MainController@categories');
+
+Route::get('/product', 'MainController@product');
