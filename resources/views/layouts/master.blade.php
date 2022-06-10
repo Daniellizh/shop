3999b524e35b2b8b1a6938e44957496631c9f47c
Daniellizh <dl.lucky.36@gmail.com>
2022-06-10 18:30:38 +0300

    createProductAndCategoryLink

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,29 +14,40 @@
     <script src="/js/bootstrap.min.js"></script>
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/starter-template.css" rel="stylesheet">
-
+    
     <script src="{{ asset('js/app.js') }}" defer></script>
-
+    
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
 </head>
 <body>
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-        <div id="navbar" class="collapse navbar-collapse">
-            <a class="navbar-brand" hred="#">Shop</a>
-    </div>
-    <div id="navbar" class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-            <li class="active"><a href="{{ route('index') }}">Home</a></li>
-            <li class="active"><a href="{{ route('categories') }}">Category</a></li>
-            <li class="active"><a href="{{ route('basket') }}">Basket</a></li>
+<nav class="navbar navbar-expand-lg bg-light container">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Shop</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{ route('index') }}">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('categories') }}">Category</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('basket') }}">Basket</a>
+          </li>
         </ul>
+      </div>
     </div>
-</nav>
+  </nav>
 
 
     <div class="container">
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', 'MainController@index')->name('index');
 Route::get('/categories', 'MainController@categories')->name('categories');
 Route::get('/{category}', 'MainController@category')->name('category');
 
-Route::get('/product', 'MainController@product')->name('product');
+Route::get('/{category}/{product?}', 'MainController@product')->name('product');
 
 Route::get('/basket', 'MainController@basket')->name('basket');
 Route::get('/basket/order', 'MainController@basketOrder')->name('basketOrder');
